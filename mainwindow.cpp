4120b113e6be57cfdd5297a770906f2234d615b7
flxw <felix.wolff@flxw.de>
2014-03-13 18:29:41 +0100

    Make window borderless

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@
 # include <likelistmodel.h>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent), ui(new Ui::MainWindow)
+    QMainWindow(parent, Qt::FramelessWindowHint), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
